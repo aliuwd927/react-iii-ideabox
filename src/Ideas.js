b3eca4fd2c6aa4a52bbc3d8b3bd00a2baf0d7225
Andy Liu <aliu927@gmail.com>
2022-10-18 23:00:31 -0700

    added github issue

--- src/Ideas.js
+++ src/Ideas.js
@@ -6,11 +6,12 @@ import { useContext } from "react";
 import ThemeContext from "./ThemeContext";
 const Ideas = () => {
   const [state, dispatch] = useContext(ThemeContext);
-
+  //Issue: Solution is missing delete function
   const deleteIdea = (id) => {
     const action = { type: "REMOVE_IDEA", id };
     dispatch(action);
   };
+
   const ideaCards = state.ideas.map((idea) => {
     return (
       <Card
